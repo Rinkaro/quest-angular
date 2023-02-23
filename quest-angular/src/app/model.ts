c5d3b694d06c3f85aa9180e001fe3d4fa64657e8
Cedric <devil-ced@hotmail.com>
2023-02-23 12:22:57 +0100

    Fomrateur, matiere at AuhtDTO dans model

--- quest-angular/src/app/model.ts
+++ quest-angular/src/app/model.ts
@@ -85,3 +85,13 @@ export class Matiere {
         this.formateur = formateur;
     }
 }
+
+export class AuthDTO {
+    login: string;
+    password: string;
+
+    constructor(login?: string, password?: string) {
+        this.login = login;
+        this.password = password;
+    }
+}
